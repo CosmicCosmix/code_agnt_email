be745294d46dc47f11cb70d404fe091f27e24c5f
Cyclohexane <pixel.abhilash@gmail.com>
2026-04-08 01:33:30 +0530

    minor comment

--- server/app.py
+++ server/app.py
@@ -20,6 +20,6 @@ def state():
     return env.state()
 def main():
     import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860) #7860 port for the openenv
 if __name__ == "__main__":
     main()    
